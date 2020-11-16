db22f81339b1238e6b3ed4c7dd552b3cf59b378a
fatpo <xihuanjianguo@gmail.com>
2020-11-16 18:16:29 +0800

    init - v2

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -5,7 +5,7 @@ CREATE TABLE `activity` (
   `title` varchar(1024) NOT NULL comment '活动标题',
   `detail` text NOT NULL comment '活动详情',
   `link_url` varchar(1024) NOT NULL comment '活动跳转链接',
-  `begin_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '活动开始时间，默认今天',
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '活动开始时间，默认今天',
   `end_time` timestamp NOT NULL DEFAULT '2037-01-01 12:00:00' comment '活动结束时间，默认遥远的未来',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -13,6 +13,4 @@ CREATE TABLE `activity` (
   UNIQUE KEY `title_idx` (`title`(20))
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-comment activity.title '';
-
-insert into activity(title, content, link_url) values ('测试title', '天气很好的内容！', 'http://www.baidu.com');
+insert into activity(title, detail, link_url) values ('测试title', '天气很好的内容！', 'http://www.baidu.com');
