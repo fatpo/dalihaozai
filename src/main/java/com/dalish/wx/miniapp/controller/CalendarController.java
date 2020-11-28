767b6119bf6241dbb93e2bb37fb02a7ac13fda97
ouyangbro <xihuanjianguo@gmail.com>
2020-11-28 21:32:36 +0800

    优化登陆接口

--- src/main/java/com/dalish/wx/miniapp/controller/CalendarController.java
+++ src/main/java/com/dalish/wx/miniapp/controller/CalendarController.java
@@ -3,15 +3,12 @@ package com.dalish.wx.miniapp.controller;
 import com.dalish.wx.miniapp.service.CalendarService;
 import com.dalish.wx.miniapp.utils.ReturnCode;
 import com.dalish.wx.miniapp.utils.ReturnObj;
-import com.dalish.wx.miniapp.vo.CalendarMonthVo;
 import com.dalish.wx.miniapp.vo.GetCalendarRspVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/calendar")
